9ab9b61bdafdb39018f6b1a83d5605ae01c4b8a4
atticus29 <mark.aaron.fisher@gmail.com>
2025-04-02 05:01:25 -0700

    remove .docusaurus directory

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/walker_local/Documents/GitHub/Symbiota-Documentation/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/walker_local/Documents/GitHub/Symbiota-Documentation/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/walker_local/Documents/GitHub/Symbiota-Documentation/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/walker_local/Documents/GitHub/Symbiota-Documentation/src/css/custom.css"),
-];
