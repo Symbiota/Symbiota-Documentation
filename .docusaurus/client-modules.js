2e63a6562009ad3c38b16eae0d84278fc5f3adda
atticus29 <mark.aaron.fisher@gmail.com>
2025-04-02 04:57:43 -0700

    remove the .docusaurus directory

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Apache24\\htdocs\\Symbiota-Docs\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Apache24\\htdocs\\Symbiota-Docs\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Apache24\\htdocs\\Symbiota-Docs\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Apache24\\htdocs\\Symbiota-Docs\\src\\css\\custom.css"),
-];
